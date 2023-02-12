481be0f2118a17449101571b28cde2fa0829b837
Rodrigo <101999518+rdGxd@users.noreply.github.com>
2023-02-12 12:49:22 -0300

    arrumei algumas coisas e comentei outras mas ainda falta o overflow do modo mobile

--- main.js
+++ main.js
@@ -3,27 +3,29 @@ const MenuClose = document.querySelector("#closeMenu");
 const nav = document.querySelector("nav");
 const main = document.querySelector("main");
 const footer = document.querySelector("footer");
-const btn = document.querySelector("#pedido")
+const btn = document.querySelector("#pedido");
 
 MenuOpen.addEventListener("click", openMenu);
 MenuClose.addEventListener("click", closeMenu);
 
+// Função que é chamada quando o usuário clica para abrir o menu no modo mobile
 function openMenu() {
-  MenuOpen.classList.add("hidden");
-  MenuClose.classList.remove("hidden");
-  nav.classList.remove("hidden");
-  main.classList.add("hidden");
-  footer.classList.add("hidden");
-  btn.removeAttribute("id", "hidden");
-  btn.classList.add("hidden");
+  MenuOpen.classList.add("isHidden");
+  MenuClose.classList.remove("isHidden");
+  nav.classList.remove("isHidden");
+  main.classList.add("isHidden");
+  footer.classList.add("isHidden");
+  btn.classList.add("isHidden");
+  btn.removeAttribute("id");
 }
 
+// Função que é chamada quando o usuário clica para fechar o menu no modo mobile
 function closeMenu() {
-  MenuOpen.classList.remove("hidden");
-  MenuClose.classList.add("hidden");
-  nav.classList.add("hidden");
-  main.classList.remove("hidden");
-  footer.classList.remove("hidden");
-  btn.classList.remove("hidden");
-  btn.setAttribute("id", "pedido")
+  MenuOpen.classList.remove("isHidden");
+  MenuClose.classList.add("isHidden");
+  nav.classList.add("isHidden");
+  main.classList.remove("isHidden");
+  footer.classList.remove("isHidden");
+  btn.classList.remove("isHidden");
+  btn.setAttribute("id", "pedido");
 }
